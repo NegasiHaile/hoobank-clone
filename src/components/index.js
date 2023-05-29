e5c8afdb05318a06a546c828d2906d02b8c73c78
NegasiHiale <NegasiHaile.Abadi@gmail.com>
2023-05-29 19:53:21 +0300

    Added clients list card

--- src/components/index.js
+++ src/components/index.js
@@ -5,5 +5,17 @@ import Business from "./Business";
 import Billing from "./Billing";
 import CardDeal from "./CardDeal";
 import Testimonials from "./Testimonials";
+import Clients from "./Clients";
+import CTA from "./CTA";
 
-export { Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonials };
+export {
+  Navbar,
+  Hero,
+  Stats,
+  Business,
+  Billing,
+  CardDeal,
+  Testimonials,
+  Clients,
+  CTA,
+};
