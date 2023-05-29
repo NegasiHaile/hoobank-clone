0ec34c0c4681486aa08f97608488f84187110197
NegasiHiale <NegasiHaile.Abadi@gmail.com>
2023-05-29 17:07:23 +0300

    Added HooBank initial setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/hoobank.svg";
+import "./style.js";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1>Vite + React + TailwindCSS</h1>
+    </div>
+  );
+}
+
+export default App;
